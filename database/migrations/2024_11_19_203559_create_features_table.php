ed8e6091fd2733bce7351716ac4736a6f840717c
abdelaziztawfekmohamed <abdelaziztawfekmohamed.com>
2024-11-20 19:58:27 +0200

    creating a feature factory

--- database/migrations/2024_11_19_203559_create_features_table.php
+++ database/migrations/2024_11_19_203559_create_features_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->longText('description')->nullable();
-            $table->foreignIdFor(User::class);
+            $table->foreignIdFor('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
