1e34f642b812e9d6a8267acfa96d7cb98bd28a36
abdelaziztawfekmohamed <abdelaziztawfekmohamed.com>
2024-11-20 22:06:27 +0200

    schema modified

--- database/migrations/2024_11_19_203859_create_upvotes_table.php
+++ database/migrations/2024_11_19_203859_create_upvotes_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Feature;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -14,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('upvotes', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor('user_id')->constrained('users')->cascadeOnDelete();
-            $table->foreignIdFor('feature_id')->constrained('features')->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Feature::class)->constrained()->cascadeOnDelete();
             $table->boolean('upvote');
         });
     }
--- database/migrations/2024_11_19_203937_create_comments_table.php
+++ database/migrations/2024_11_19_203937_create_comments_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Feature;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -15,8 +16,8 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->string('comment');
-            $table->foreignIdFor('user_id')->constrained('users')->cascadeOnDelete();
-            $table->foreignIdFor('feature_id')->constrained('features')->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Feature::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
